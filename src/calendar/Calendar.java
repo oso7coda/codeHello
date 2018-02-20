0742c4bfb423b0a04c7295cc99b3f955186fe1a8
jojeongwoo <jojeongwoo@DESKTOP-TNSODJA>
2018-02-20 23:06:42 +0900

    update

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -3,12 +3,13 @@ package calendar;
 import java.util.Scanner;
 
 public class Calendar {
-	
-	private final int[] MAX_DAY = {31,28,31,30,31,30,31,31,30,31,30,31};
+
+	private final int[] MAX_DAY = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
 	public int maxDaysOfMonth(int month) {
-		return MAX_DAY[month-1];
+		return MAX_DAY[month - 1];
 	}
-	
+
 	public void printSample() {
 		System.out.println("일    월    화    수    목    금    토");
 		System.out.println("--------------------");
@@ -17,24 +18,20 @@ public class Calendar {
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
 	}
-	
-	public static void main(String[] args) {
 
+	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
 		System.out.println("달을 입력하세요");
-		
+
 		Calendar cal = new Calendar();
-		
-		
-		int month = sc.nextInt();//한큐에 넣기
-		
-//		int[] maxDay = {31,28,31,30,31,30,31,31,30,31,30,31};
-		
-		System.out.printf("%d월은 %d일까지 있습니다;\n",month,cal.maxDaysOfMonth(month));
+
+		int month = sc.nextInt();// 한큐에 넣기
+
+		// int[] maxDay = {31,28,31,30,31,30,31,31,30,31,30,31};
+
+		System.out.printf("%d월은 %d일까지 있습니다;\n", month, cal.maxDaysOfMonth(month));
 		sc.close();
-		
-		
-		
+
 	}
 }
